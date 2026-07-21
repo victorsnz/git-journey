31d4ed47f66d974b378f7c690d4b88173f33fd20
Victor Sanchez <victor.sanchez@desarrollosnea.com>
2026-07-21 14:21:47 -0300

    chore: add easter egg on console

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,15 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles/globals.css";
 
+console.log(
+  "%c Bienvenido a Git Journey 🚀\n\n%c Si estás leyendo esto seguramente seas desarrollador.\n\n%c Gracias por visitar el proyecto.\n\n%c Repositorio:\n%c https://github.com/victorsnz/git-journey",
+  "font-size: 20px; font-weight: bold; color: #3bce6b;",
+  "color: #eeeef0;",
+  "color: #eeeef0;",
+  "color: #88888b;",
+  "color: #3bce6b; text-decoration: underline;"
+);
+
 createRoot(document.getElementById("app")!).render(
   <StrictMode>
     <App />
