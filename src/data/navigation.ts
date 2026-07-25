aeee74810b8417570b7dd932e73b5f541c7c108a
Victor Sanchez <victor.sanchez@desarrollosnea.com>
2026-07-25 01:05:32 -0300

    test para merge

--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -13,4 +13,5 @@ export const navigation: NavSection[] = [
   { id: "buenas-practicas", label: "Buenas prácticas" },
   { id: "cheat-sheet", label: "Cheat Sheet (machetito)" },
   { id: "fuentes", label: "Fuentes" },
+  { id: "test", label: "Test" },
 ];
